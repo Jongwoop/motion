d527553c7e921a4dc5331c24c65a5ae29a942b53
john <58589069+Jongwoop@users.noreply.github.com>
2021-07-06 13:12:35 +0300

    Made class: App, PageComponent

--- dist/components/page.js
+++ dist/components/page.js
@@ -2,6 +2,9 @@ export class PageComponent {
     constructor() {
         this.element = document.createElement("ul");
         this.element.setAttribute("class", "page");
-        this.element.textContent = "this is PageComponent";
+        this.element.textContent = "This is PageComponent";
+    }
+    attachTo(parent, position = "afterbegin") {
+        parent.insertAdjacentElement(position, this.element);
     }
 }
