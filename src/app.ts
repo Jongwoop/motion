13403bcfb1015b97954df2c0d3e3a167953e8aec
john <58589069+Jongwoop@users.noreply.github.com>
2021-07-07 13:14:18 +0300

    Add PageItemComponent

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,12 @@
 import { NoteComponent } from "./components/page/item/note.js";
 import { ImageComponent } from "./components/page/item/image.js";
-import { PageComponent } from "./components/page/page.js";
+import { Composable, PageComponent } from "./components/page/page.js";
 import { TodoComponent } from "./components/page/item/todo.js";
 import { VideoComponent } from "./components/page/item/video.js";
+import { Component } from "./components/component.js";
 
 class App {
-  private readonly page: PageComponent;
+  private readonly page: Component & Composable;
   constructor(appRoot: HTMLElement) {
     this.page = new PageComponent();
     this.page.attachTo(appRoot);
@@ -14,19 +15,19 @@ class App {
       "Image Title",
       "https://picsum.photos/600/300"
     );
-    image.attachTo(appRoot, "beforeend");
+    this.page.addChild(image);
 
     const note = new NoteComponent("Note Title", "Note Body");
-    note.attachTo(appRoot, "beforeend");
+    this.page.addChild(note);
 
     const todo = new TodoComponent("todo Title", "todo Body");
-    todo.attachTo(appRoot, "beforeend");
+    this.page.addChild(todo);
 
     const video = new VideoComponent(
       "video Title",
       "https://www.youtube.com/watch?v=aEPt7mNcUNU"
     );
-    video.attachTo(appRoot, "beforeend");
+    this.page.addChild(video);
   }
 }
 
