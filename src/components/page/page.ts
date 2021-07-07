45d63f8e9e8546947d8b530964bda276a04db6a8
john <58589069+Jongwoop@users.noreply.github.com>
2021-07-07 15:34:07 +0300

    Implement removing sections from the PageComponent

--- src/components/page/page.ts
+++ src/components/page/page.ts
@@ -4,10 +4,9 @@ export interface Composable {
   addChild(child: Component): void;
 }
 
-class PageItemComponent
-  extends BaseComponent<HTMLElement>
-  implements Composable
-{
+type OnCloseListner = () => void;
+class PageItemComponent extends BaseComponent<HTMLElement> implements Composable {
+  private closeListener?: OnCloseListner;
   constructor() {
     super(`
     <li class="page-item">
@@ -16,19 +15,21 @@ class PageItemComponent
         <button class="close">X</button>
       </div>
     </li>`);
+    const closeBtn = this.element.querySelector(".close")! as HTMLButtonElement;
+    closeBtn.onclick = () => {
+      this.closeListener && this.closeListener();
+    };
   }
   addChild(child: Component) {
-    const container = this.element.querySelector(
-      ".page-item__body"
-    )! as HTMLElement;
+    const container = this.element.querySelector(".page-item__body")! as HTMLElement;
     child.attachTo(container);
+  }
+  setOnCloseListner(listener: OnCloseListner) {
+    this.closeListener = listener;
   }
 }
 
-export class PageComponent
-  extends BaseComponent<HTMLUListElement>
-  implements Composable
-{
+export class PageComponent extends BaseComponent<HTMLUListElement> implements Composable {
   constructor() {
     super('<ul class"page"> this is Page Component');
   }
@@ -36,5 +37,8 @@ export class PageComponent
     const item = new PageItemComponent();
     item.addChild(section);
     item.attachTo(this.element, "beforeend");
+    item.setOnCloseListner(() => {
+      item.removeFrom(this.element);
+    });
   }
 }
