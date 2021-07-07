2064de42552062fd2742eba3bcd18bfb7b0a96cd
john <58589069+Jongwoop@users.noreply.github.com>
2021-07-07 16:30:09 +0300

    Inject PageItemConstructor to the PageComponent for testability

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,6 @@
 import { NoteComponent } from "./components/page/item/note.js";
 import { ImageComponent } from "./components/page/item/image.js";
-import { Composable, PageComponent } from "./components/page/page.js";
+import { Composable, PageComponent, PageItemComponent } from "./components/page/page.js";
 import { TodoComponent } from "./components/page/item/todo.js";
 import { VideoComponent } from "./components/page/item/video.js";
 import { Component } from "./components/component.js";
@@ -8,13 +8,10 @@ import { Component } from "./components/component.js";
 class App {
   private readonly page: Component & Composable;
   constructor(appRoot: HTMLElement) {
-    this.page = new PageComponent();
+    this.page = new PageComponent(PageItemComponent);
     this.page.attachTo(appRoot);
 
-    const image = new ImageComponent(
-      "Image Title",
-      "https://picsum.photos/600/300"
-    );
+    const image = new ImageComponent("Image Title", "https://picsum.photos/600/300");
     this.page.addChild(image);
 
     const note = new NoteComponent("Note Title", "Note Body");
@@ -23,10 +20,7 @@ class App {
     const todo = new TodoComponent("todo Title", "todo Body");
     this.page.addChild(todo);
 
-    const video = new VideoComponent(
-      "video Title",
-      "https://www.youtube.com/watch?v=aEPt7mNcUNU"
-    );
+    const video = new VideoComponent("video Title", "https://www.youtube.com/watch?v=aEPt7mNcUNU");
     this.page.addChild(video);
   }
 }
--- src/components/page/page.ts
+++ src/components/page/page.ts
@@ -5,7 +5,14 @@ export interface Composable {
 }
 
 type OnCloseListner = () => void;
-class PageItemComponent extends BaseComponent<HTMLElement> implements Composable {
+interface SectionContainer extends Component, Composable {
+  setOnCloseListner(listener: OnCloseListner): void;
+}
+
+type SectionContainerConstructor = {
+  new (): SectionContainer;
+};
+export class PageItemComponent extends BaseComponent<HTMLElement> implements SectionContainer {
   private closeListener?: OnCloseListner;
   constructor() {
     super(`
@@ -30,11 +37,11 @@ class PageItemComponent extends BaseComponent<HTMLElement> implements Composable
 }
 
 export class PageComponent extends BaseComponent<HTMLUListElement> implements Composable {
-  constructor() {
+  constructor(private pageItemConstructor: SectionContainerConstructor) {
     super('<ul class"page"> this is Page Component');
   }
   addChild(section: Component) {
-    const item = new PageItemComponent();
+    const item = new this.pageItemConstructor();
     item.addChild(section);
     item.attachTo(this.element, "beforeend");
     item.setOnCloseListner(() => {
